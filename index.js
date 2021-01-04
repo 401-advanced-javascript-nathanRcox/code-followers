a58f84c191ceffe41172f01e0ee5a2116c33ee71
Garrett Cintron <grrttcintron@gmail.com>
2021-01-04 15:20:13 -0800

    prompts fixed

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const prompts = require('prompts');
 require('dotenv').config();
 const server = require('./src/server');
 
-// (async () => {
-//   const response = await prompts({
-//     type: 'string',
-//     name: 'value',
-//     message: 'Hello new user! What is your name?',
-//   });
-//   console.log(response);
-// })();
+(async () => {
+  const response = await prompts({
+    type: 'text',
+    name: 'value',
+    message: 'Hello new user! What is your name?',
+  });
+  console.log(response);
+})();
 
 
 server.start(process.env.PORT);
