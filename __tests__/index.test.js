0b21b3c396e01251f7a280156fe811b113a6047b
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-01-04 17:11:15 -0700

    added basic server tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,2 +1,20 @@
 'use strict';
 
+xdescribe('Console Logs', () => {
+  let consoleSpy;
+  beforeEach(() => {
+    // Attach to the console
+    consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+  });
+
+  afterEach(() =>{
+    // put the console back
+    consoleSpy.mockRestore();
+  });
+
+  it('picks up an order', () => {
+    caps.emit('pickup', {orderID: 1});
+    setTimeout(() => { expect(consoleSpy).toHaveBeenCalled(); }, 1000);
+  });
+
+});
